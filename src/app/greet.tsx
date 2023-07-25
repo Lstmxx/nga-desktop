26e5c535fcf9f40ce67dd4f7ee7b9bfadab319d8
--local <--local>
2023-07-25 16:56:16 +0800

    init

---
+++ src/app/greet.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useCallback, useEffect } from 'react';
+
+
+export default function Greet() {
+
+  const cb = useCallback(async () => {
+    const invoke = (await import('@tauri-apps/api')).invoke;
+    invoke<string>('greet', { name: 'Next.js' })
+      .then(console.log)
+      .catch(console.error);
+  },[])
+
+  useEffect(() => {
+    if (cb) {
+      cb();
+    }
+  }, [cb]);
+
+  return <></>
+}
